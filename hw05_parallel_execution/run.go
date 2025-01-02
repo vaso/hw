f054e58b5586cb85fd733a8d2a8092e4d77283c7
Сергеев Василий Леонидович <vsergeev@roowix.com>
2025-01-02 12:03:17 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"log"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -9,7 +11,90 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+func Run(tasks []Task, n int, m int) error {
+	if m <= 0 {
+		return ErrErrorsLimitExceeded
+	}
+
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+
+	tChan := make(chan Task)          // channel with array of tasks to complete
+	errChan := make(chan struct{}, 1) // flag to stop all go routines due to error limit exceeding
+	errCountChan := make(chan int, 1) // channel with error counter value
+	errCountChan <- 0
+
+	log.Println("start Run")
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		producer(tasks, tChan, errChan)
+	}()
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			consumer(tChan, errChan, errCountChan, &mu, m)
+		}()
+	}
+	wg.Wait()
+	localErrCount := <-errCountChan
+	if localErrCount >= m {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
+}
+
+func producer(tasks []Task, tChan chan<- Task, errChan chan struct{}) {
+	for _, task := range tasks {
+		select {
+		case <-errChan: // read errChan for exit flag
+			close(tChan)
+			writeToErrChan(errChan)
+			return
+		case tChan <- task: // send task to tChan, blocking
+			continue
+		}
+	}
+	close(tChan)
+}
+
+func writeToErrChan(errChan chan struct{}) {
+	select {
+	case errChan <- struct{}{}:
+	default:
+	}
+}
+
+func consumer(tChan <-chan Task, errChan chan struct{}, errCountChan chan int, mu *sync.Mutex, maxErrCount int) {
+	for {
+		select {
+		case <-errChan:
+			return
+		case task, ok := <-tChan: // read task to execute, handle task
+			if !ok {
+				return
+			}
+			err := task()
+			if err == nil {
+				continue
+			}
+
+			localErrCounter := getAndIncrementErrCount(errCountChan, mu)
+			if localErrCounter >= maxErrCount {
+				writeToErrChan(errChan)
+				return
+			}
+		}
+	}
+}
+
+func getAndIncrementErrCount(errCountChan chan int, mu *sync.Mutex) int {
+	mu.Lock()
+	defer mu.Unlock()
+	localErrCounter := <-errCountChan
+	localErrCounter++
+	errCountChan <- localErrCounter // increment errCount in lock
+	return localErrCounter
 }

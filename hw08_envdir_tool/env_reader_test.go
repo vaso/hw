fb48a45de5417d37f89876cbd1a0114fc8bdbd1e
Сергеев Василий Леонидович <vsergeev@roowix.com>
2025-02-19 22:12:14 +0300

    HW08: add tests

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require" //nolint:all
+)
 
 func TestReadDir(t *testing.T) {
-	t.Skip()
+	t.Run("test example env", func(t *testing.T) {
+		env, err := ReadDir("testdata/env")
+		require.NoError(t, err)
+
+		expectedEnv := Environment{
+			"BAR": EnvValue{
+				Value:      "bar",
+				NeedRemove: false,
+			},
+			"EMPTY": EnvValue{
+				Value:      "",
+				NeedRemove: false,
+			},
+			"FOO": EnvValue{
+				Value:      "   foo\nwith new line",
+				NeedRemove: false,
+			},
+			"HELLO": EnvValue{
+				Value:      "\"hello\"",
+				NeedRemove: false,
+			},
+			"UNSET": EnvValue{
+				Value:      "",
+				NeedRemove: true,
+			},
+		}
+		require.Equal(t, expectedEnv, env)
+	})
 }

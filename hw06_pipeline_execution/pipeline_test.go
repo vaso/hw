f6371b99908562e8b7186eede119e77e5bc561e1
Сергеев Василий Леонидович <vsergeev@roowix.com>
2025-02-19 21:23:43 +0300

    HW06: added unit-test

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -93,6 +93,27 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("empty stage list case", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		var stages []Stage
+
+		result := make([]int, 0, 10)
+		for s := range ExecutePipeline(in, nil, stages...) {
+			result = append(result, s.(int))
+		}
+
+		require.Equal(t, data, result)
+	})
 }
 
 func TestAllStageStop(t *testing.T) {

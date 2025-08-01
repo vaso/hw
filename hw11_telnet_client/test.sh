e8737e81dacbd1c7623bb96591ffe53f068c78be
Сергеев Василий Леонидович <vsergeev@roowix.com>
2025-08-01 19:34:30 +0300

    hw11: fix test.sh

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -5,11 +5,11 @@ go build -o go-telnet
 rm -f nc.out
 rm -f telnet.out
 
-(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >nc.out &
+(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 35353 >nc.out &
 NC_PID=$!
 
 sleep 1
-(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost 4242 >telnet.out &
+(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost 35353 >telnet.out &
 TL_PID=$!
 
 sleep 5
@@ -19,7 +19,7 @@ kill ${NC_PID} 2>/dev/null || true
 function fileEquals() {
   local fileData
   fileData=$(cat "$1")
-  [ "${fileData}" = "${2}" ] || (echo -e "unexpected output, $1:\n${fileData}:\n${2}" && ls -lsa && cat "$1" && exit 1)
+  [ "${fileData}" = "${2}" ] || (echo -e "unexpected output, $1:\n${fileData}:\n${2}" && ls -lsa $1 && cat "$1" && exit 1)
 }
 
 expected_nc_out=$'I\nam\nTELNET client'

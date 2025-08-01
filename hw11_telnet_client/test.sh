020f14247be17ae352fa99310c63020432032b26
Сергеев Василий Леонидович <vsergeev@roowix.com>
2025-08-01 20:31:16 +0300

    hw11: fix test.sh

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -9,11 +9,11 @@ port=$(shuf -i 40000-50000 -n 1)
 rm -f ${nc_out}
 rm -f ${telnet_out}
 
-(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost ${port} > ${nc_out} &
+(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | eval "nc -l localhost ${port} > ${nc_out}" &
 NC_PID=$!
 
 sleep 1
-(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost ${port} > ${telnet_out} &
+(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | eval "./go-telnet --timeout=5s localhost ${port} > ${telnet_out}" &
 TL_PID=$!
 
 sleep 5
@@ -23,7 +23,7 @@ kill ${NC_PID} 2>/dev/null || true
 function fileEquals() {
   local fileData
   fileData=$(cat "$1")
-  [ "${fileData}" = "${2}" ] || (echo -e "unexpected output, $1:\n${fileData}:\n${2}" && ls -lsa $1 && cat "$1" && exit 1)
+  [ "${fileData}" = "${2}" ] || (echo -e "unexpected output, $1:\n${fileData}:\n${2}" && ls -lsa $1 && cat "$1" && rm -f $1 && exit 1)
 }
 
 expected_nc_out=$'I\nam\nTELNET client'
